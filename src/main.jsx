f5ba585667b242f7bdde82fee8f337bf111be723
eze021188 <30888212+eze021188@users.noreply.github.com>
2025-05-19 00:26:44 -0600

    Configurar variables de entorno

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,11 @@ import { I18nextProvider } from 'react-i18next'
 import './index.css'
 import i18n from './i18n/config'
 import App from './App.jsx'
-import * as Sentry from '@sentry/react'
-
-// Inicializa Sentry
-if (import.meta.env.PROD) {
-  Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN,
-    integrations: [
-      new Sentry.BrowserTracing(),
-      new Sentry.Replay(),
-    ],
-  })
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Sentry.ErrorBoundary fallback={<p>Ha ocurrido un error</p>}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </Sentry.ErrorBoundary>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </StrictMode>,
 )
